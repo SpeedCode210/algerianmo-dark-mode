9e7354d1a88c7abe556255e545b567257cdae504
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2023-02-10 20:34:01 +0000

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,70 +4,71 @@
 // @include     *algerianmo.com/*
 // @downloadURL https://github.com/SpeedCode210/algerianmo-dark-mode/raw/main/script.user.js
 // @icon http://www.algerianmo.com/static/images/favicon.ico
-// @version     1.3
+// @version     1.3.2
 // @author      Raouf Ould Ali / SpeedCode#0050
 // @description 1/25/2023, 5:34:04 PM
 // ==/UserScript==
 
 document.getElementsByTagName('head')[0].innerHTML += `
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@forevolve/bootstrap-dark@1.0.0/dist/css/bootstrap-dark.min.css" title="BoostrapDark"/>
-<style title="DarkMode">
-  .math-output { background-color: #343434; }
+<style>
+  .dark .math-output { background-color: #343434; }
 .table-danger, .table-danger>td, .table-danger>th {
     background-color: #631C1C;
     color: white;
 }
 
-.table-success, .table-success>td, .table-success>th {
+.dark .table-success, .table-success>td, .table-success>th {
     background-color: #3A7722;
     color: white;
 }
-  .table-warning, .table-warning>td, .table-warning>th {
+  .dark .table-warning, .table-warning>td, .table-warning>th {
     background-color: #F4BA24;
     color: black;
 }
-  .logo-amo{
+  .dark .logo-amo{
     filter: brightness(0) invert(1);
   }
-  body{
+  .dark {
     background-color: #1C1D21;
   }
-  .force-black{
+  .dark .force-black{
     color : black!important;
   }
-  .force-black > h3 > a, .force-black > a{
+  .dark .force-black > h3 > a, .force-black > a{
     color: black!important;
   }
 
-  .force-white{
+  .dark .force-white{
     color : white!important;
   }
-  .force-white > h3 > a, .force-white > a{
+  .dark .force-white > h3 > a, .force-white > a{
     color: white!important;
   }
 
-  .solution-card img {
+  .dark .solution-card img {
   filter: invert() contrast(1.5);
   }
 
-  .green-nav{
+  .dark .green-nav{
     background-color:#105636!important;
   }
 
-  .head-green  { background-color : #274F17!important; }
-  .head-black  { background-color : #3B3947!important; }
-  .body-green  { background-color : #3A7722!important; }
-  .unknown     { background-color : rgb(34 34 34)!important; }
-  .body-black  { background-color : #312F37!important; }
-  .head-yellow { background-color : #BB5D25!important; }
-  .body-yellow { background-color : #E38725!important; }
-  .head-red    { background-color : #631C1C!important; }
-  .body-red    { background-color : #772222!important; }
+  .dark .head-green  { background-color : #274F17 !important; }
+  .dark .head-black  { background-color : #3B3947 !important; }
+  .dark .body-green  { background-color : #3A7722 !important; }
+  .dark .unknown     { background-color : rgb(34 34 34) !important; }
+  .dark .body-black  { background-color : #312F37 !important; }
+  .dark .head-yellow { background-color : #BB5D25 !important; }
+  .dark .body-yellow { background-color : #E38725 !important; }
+  .dark .head-red    { background-color : #631C1C !important; }
+  .dark .body-red    { background-color : #772222 !important; }
 </style>
 `
 
 document.getElementsByTagName("nav")[0].classList.add("green-nav");
 
+let body = document.getElementsByTagName("body")[0];
 
 let cards = document.getElementsByTagName("div");
 
@@ -118,19 +119,26 @@ for(let i =0; i < imgs.length; i++){
 let nav = document.getElementsByClassName("navbar-nav")[0];
 nav.innerHTML = nav.innerHTML +
   '<button type="button" id="switch-dark" class="btn btn-dark">Dark Mode Toggle</button>';
-var darkModeStylesheet = [].slice.call(document.styleSheets).filter(s=>s.title == "DarkMode")[0];
+
 var boostrapDarkStylesheet = [].slice.call(document.styleSheets).filter(s=>s.title == "BoostrapDark")[0];
 document.getElementById("switch-dark").addEventListener("click", ()=>{
-  darkModeStylesheet.disabled = !darkModeStylesheet.disabled;
   boostrapDarkStylesheet.disabled = !boostrapDarkStylesheet.disabled;
   localStorage["dark"] = localStorage["dark"] == "TRUE" ? "FALSE" : "TRUE";
+  if(localStorage["dark"] == "TRUE"){
+    body.classList.add("dark");
+  }
+  else{
+    body.classList.remove("dark");
+  }
 });
 
 if(!localStorage["dark"]){
   localStorage["dark"] = "TRUE";
 }
 
-darkModeStylesheet.disabled = localStorage["dark"] != "TRUE";
+if(localStorage["dark"] == "TRUE"){
+  body.classList.add("dark");
+}
 boostrapDarkStylesheet.disabled = localStorage["dark"] != "TRUE";
 
 
